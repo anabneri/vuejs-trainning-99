d1297838831648d244ee88b82c9b6061db6971ef
anabneri <ananeri32@hotmail.com>
2020-11-12 18:45:05 -0300

    feat(): initial commit

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <a>Title</a>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    .a {
+        color: red;
+    }
+</style>
